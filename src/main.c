2b6412214e913ece431176b2d0babd868722c6cd
Thomas Von Ascheberg <tvonasc@a-018818.ups.u-psud.fr>
2019-01-16 11:14:14 +0100

    Add main file

---
+++ src/main.c
@@ -0,0 +1,79 @@
+#include "structures.h"
+#include "grammar.tab.h"
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+
+
+// Verbose
+int verbose;
+int errorCode;
+
+extern int yyparse();
+extern int yylineno;
+
+/* Appel:
+ *   tp [-option]* programme.txt
+ * Les options doivent apparaitre avant le nom du fichier du programme.
+ * Options: -[eE] -[vV] -[hH?]
+ */
+int main(int argc, char **argv) {
+  int fi;
+  int i, res;
+
+  for(i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      switch (argv[i][1]) {
+      case 'v': case 'V':
+	verbose = TRUE; continue;
+      case 'e': case 'E':
+	fprintf(stderr, "Syntax: tp -e -v program.txt\n");
+	exit(USAGE_ERROR);
+      default:
+	fprintf(stderr, "Error: Unknown Option: %c\n", argv[i][1]);
+	exit(USAGE_ERROR);
+      }
+    } else break;
+  }
+
+  if (i == argc) {
+    fprintf(stderr, "Error: Program file is missing\n");
+    exit(USAGE_ERROR);
+  }
+
+  if ((fi = open(argv[i++], O_RDONLY)) == -1) {
+    fprintf(stderr, "Error: Cannot open %s\n", argv[i-1]);
+    exit(USAGE_ERROR);
+  }
+
+  /* redirige l'entree standard sur le fichier... */
+  close(0); dup(fi); close(fi);
+
+  if (i < argc) { /* fichier dans lequel lire les valeurs pour get() */
+    fprintf(stderr, "Error: extra argument: %s\n", argv[i]);
+    exit(USAGE_ERROR);
+  }
+
+  /* Lance l'analyse syntaxique de tout le source, qui appelle yylex au fur
+   * et a mesure. Execute les actions semantiques en parallele avec les
+   * reductions.
+   * yyparse renvoie 0 si le source est syntaxiquement correct, une valeur
+   * differente de 0 en cas d'erreur syntaxique (eventuellement dues a des
+   * erreurs lexicales).
+   * Comme l'interpretation globale est automatiquement lancee par les actions
+   * associees aux reductions, une fois que yyparse a termine il n'y
+   * a plus rien a faire (sauf fermer les fichiers)
+   * Si le code du programme contient une erreur, on bloque l'evaluation.
+   * S'il n'y a que des erreurs contextuelles on essaye de ne pas s'arreter
+   * a la premiere mais de continuer l'analyse pour en trovuer d'autres, quand
+   * c'est possible.
+   */
+  res = yyparse();
+  if (res == 0 && errorCode == NO_ERROR) return 0;
+  else {
+    return res ? SYNTAX_ERROR : errorCode;
+  }
+}
