543efad89e531b0dfab5aae0250a277455a9ce26
Konrad Powęska <konradpoweska5@gmail.com>
2019-01-21 21:41:52 +0100

    Update semantics

--- src/semantics.c
+++ src/semantics.c
@@ -1,9 +1,41 @@
+#include <stdio.h>
 #include "semantics.h"
+#include "common.h"
+#include "init.h" // access to globals
 
-ClassP getType(TreeP expr) {
-  return NIL(Class);
+
+ClassP getTypeITE(TreeP expr) {
+  if(expr->opLabel != L_IFTHENELSE) exit(UNEXPECTED);
+
+  const ClassP thenType = getType(expr->u.children[1]);
+  const ClassP elseType = getType(expr->u.children[2]);
+
+  if(thenType == elseType) return thenType;
+  else {
+    printError("different types in then & else\n");
+    exit(CONTEXT_ERROR);
+  }
 }
 
+/* Returns type of an expression */
+ClassP getType(TreeP expr) {
+  switch(expr->opLabel) {
+    case L_IFTHENELSE: return getTypeITE(expr);
+    case L_CONSTINT: return &Integer;
+    case L_CONSTSTR: return &String;
+    default: return NIL(Class);
+  }
+}
+
+/* Returns TRUE if targetType is supertype of thisType */
 bool derivesType(ClassP thisType, ClassP targetType) {
-  return FALSE;
+  if(targetType == NIL(Class)) exit(UNEXPECTED);
+
+  if(thisType == NIL(Class)) return FALSE;
+  // reached top of hierarchy -> no match
+
+  if(thisType == targetType) return TRUE;
+
+  return derivesType(thisType->superClass, targetType);
+  // launch recursive with upper level
 }
--- src/semantics.h
+++ src/semantics.h
@@ -1,4 +1,11 @@
+#ifndef SEMANTICS_H
+#define SEMANTICS_H
+
 #include "structures.h"
 
+
 ClassP getType(TreeP expr);
+
 bool derivesType(ClassP thisType, ClassP targetType);
+
+#endif
