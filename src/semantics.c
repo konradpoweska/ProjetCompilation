b9a4c998acd930b4e9f377653fe40274372a9c67
cherilyn <cherilyn@cherilyndeMacBook-Pro.local>
2019-01-24 12:49:40 +0100

    add types for getType

--- src/semantics.c
+++ src/semantics.c
@@ -17,12 +17,41 @@ ClassP getTypeITE(TreeP expr) {
   }
 }
 
+ClassP getTypeNEW(TreeP expr) {
+  if(expr->opLabel != L_NEW) exit(UNEXPECTED);
+
+  return expr->u.children[0]->u.class;
+}
+
+ClassP getTypeSELEC(TreeP expr) {
+  if(expr->opLabel != L_NEW) exit(UNEXPECTED);
+
+  return NULL; //A FINIR
+
+}
+
+ClassP getTypeMSG(TreeP expr) {
+  if(expr->opLabel != L_MESSAGE) exit(UNEXPECTED);
+  
+  return NULL; //A FINIR
+}
+
+ClassP getTypeCAST(TreeP expr) {
+  if(expr == NULL || expr->opLabel != L_CAST) exit(UNEXPECTED);
+
+  return expr->u.children[0]->u.class;
+}
+
 /* Returns type of an expression */
 ClassP getType(TreeP expr) {
   switch(expr->opLabel) {
     case L_IFTHENELSE: return getTypeITE(expr);
+    case L_NEW: return getTypeNEW(expr);
     case L_CONSTINT: return &Integer;
     case L_CONSTSTR: return &String;
+    case L_SELECTION: return getTypeSELEC(expr);
+    case L_MESSAGE: return getTypeMSG(expr);
+    case L_CAST: return getTypeCAST(expr);
     default: return NIL(Class);
   }
 }
